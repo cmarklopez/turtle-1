d45af6508e3db513fc300e48d3bb7eb32d56dec4
Mark Lopez <cmarklopez@gmail.com>
2022-06-22 21:00:50 -0500

    Renaming new_collections.py back to collections.py

---
+++ new_collections.py
@@ -0,0 +1,74 @@
+
+'''
+li = [1,2,3,4,6]
+
+print(li)
+
+s = len(li)
+print(s)
+
+t = li.pop()
+print(t)
+
+print(li)
+
+li.append(5)
+print(li)
+
+li2 = li
+
+print(li2)
+
+print(li2 is li)
+'''
+
+'''
+This is a multi-linee
+string that I am using
+from a comment!!!
+'''
+
+
+'''
+
+li.pop()
+
+print(li2)
+
+li3 = [10,11,12]
+
+li.extend(li3)
+
+print(li)
+
+li.clear()
+
+print(li)
+
+c = li.count(1)
+
+print(c)
+
+
+print('s' in 'sfdswe')
+
+test = 'ssrffrefsfdewessss'
+
+if('s' in test):
+    print(test.count('s'))
+
+
+a = [1,2]
+b = [2,3]
+
+print(a+b)
+
+
+'''
+
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+
+)ast = sports[-3:]
+
+print(last)
+print('goodbye')
